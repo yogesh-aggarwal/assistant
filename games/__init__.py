cfb78bb9ca5c164f44704da8572f34a04e2497af
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2019-09-08 23:45:40 +0530

    Modified game api data (trivia)

--- games/__init__.py
+++ games/__init__.py
@@ -2,47 +2,30 @@
 Game extention for Jarvis AI Project
 """
 
-choice = [
-    {
-        "name": "opentdb",
-        "category": "trivia",
-        "api": {
-            "host": "https://opentdb.com/api.php?",
-            "attributes": {
-                "questionAmount": {
-                    "key": "amount",
-                    "range": "50"
+choice = {
+    "trivia": [
+        {
+            "name": "opentdb",
+            "category": "trivia",
+            "api": {
+                "host": "https://opentdb.com/api.php?",
+                "attributes": {
+                    "questionAmount": {"key": "amount", "range": "50"},
+                    "questionCategory": {"key": "category", "range": "9-32"},
+                    "questionDifficulty": {
+                        "key": "difficulty",
+                        "range": ["easy", "medium", "difficult"],
+                    },
+                    "questionType": {"key": "type", "range": ["multiple", "boolean"]},
+                    "questionEncoding": {
+                        "key": "encoding",
+                        "range": ["url3986", "base64"],
+                    },
                 },
-                "questionCategory": {
-                    "key": "category",
-                    "range": "9-32"
-                },
-                "questionDifficulty": {
-                    "key": "difficulty",
-                    "range": [
-                        "easy",
-                        "medium",
-                        "difficult"
-                    ]
-                },
-                "questionType": {
-                    "key": "type",
-                    "range": [
-                        "multiple",
-                        "boolean"
-                    ]
-                },
-                "questionEncoding": {
-                    "key": "encoding",
-                    "range": [
-                        "url3986",
-                        "base64"
-                    ]
-                }
-            }
+            },
         }
-    }
-]
+    ]
+}
 
 
 def init():
